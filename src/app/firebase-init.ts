7f05a089a8e40b7ffdee0aa10c4b936c07a1ff6c
Tsvetomir <1hundredheads@gmail.com>
2024-12-17 22:54:36 +0200

    unused filed deleted

--- src/app/firebase-init.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from './config/firebase-config';
-
-// Инициализация на Firebase
-const app = initializeApp(firebaseConfig);
-export { app };
